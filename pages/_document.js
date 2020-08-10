c83a778c76b269a959b60cdee89a8f47fc0625f2
ahmadsupri <supri@comeapp.id>
2020-08-10 09:24:20 +0700

    Add meta description and keyword

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,15 @@ class KMMBDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="KMMB 2020 merupakan acara yang diadakan oleh BEM KM Fakultas Psikologi Universitas Airlangga dengan memberikan wadah bagi mahasiswa untuk mengaktualisasikan diri, menampilkan kemampuannya, serta menginisiasi dukungan dan kontribusi mahasiswa dalam peranannya dengan memberikan ruang gerak bagi mahasiswa untuk berinovasi dan berkreasi."
+          />
+          <meta
+            name="keywords"
+            content="konferensi, mahasiswa, merdeka, kmmb, indonesia, kompetisi, competition, talkshow, breakourboundaries"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
@@ -13,23 +22,82 @@ class KMMBDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
-          <link rel="apple-touch-icon" sizes="57x57" href="images/icon/apple-icon-57x57.png"/>
-          <link rel="apple-touch-icon" sizes="60x60" href="images/icon/apple-icon-60x60.png"/>
-          <link rel="apple-touch-icon" sizes="72x72" href="images/icon/apple-icon-72x72.png"/>
-          <link rel="apple-touch-icon" sizes="76x76" href="images/icon/apple-icon-76x76.png"/>
-          <link rel="apple-touch-icon" sizes="114x114" href="images/icon/apple-icon-114x114.png"/>
-          <link rel="apple-touch-icon" sizes="120x120" href="images/icon/apple-icon-120x120.png"/>
-          <link rel="apple-touch-icon" sizes="144x144" href="images/icon/apple-icon-144x144.png"/>
-          <link rel="apple-touch-icon" sizes="152x152" href="images/icon/apple-icon-152x152.png"/>
-          <link rel="apple-touch-icon" sizes="180x180" href="images/icon/apple-icon-180x180.png"/>
-          <link rel="icon" type="image/png" sizes="192x192"  href="images/icon/android-icon-192x192.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="images/icon/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="96x96" href="images/icon/favicon-96x96.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="images/icon/favicon-16x16.png"/>
-          <link rel="manifest" href="images/icon/manifest.json"/>
-          <meta name="msapplication-TileColor" content="#ffffff"/>
-          <meta name="msapplication-TileImage" content="images/icon/ms-icon-144x144.png"/>
-          <meta name="theme-color" content="#ffffff"/>
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="images/icon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="images/icon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="images/icon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="images/icon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="images/icon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="images/icon/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="images/icon/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="images/icon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="images/icon/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="images/icon/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="images/icon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="images/icon/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="images/icon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="images/icon/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-TileImage"
+            content="images/icon/ms-icon-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
