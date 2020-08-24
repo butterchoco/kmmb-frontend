87f6a67c33d769b01584ed87e1fbfcb4a547a33a
ahmadsupri <supri@comeapp.id>
2020-08-24 18:17:11 +0700

    Fix carousel speakers

--- components/SpeakersCarousel.js
+++ components/SpeakersCarousel.js
@@ -2,20 +2,31 @@ import Slider from "react-slick";
 
 const SpeakersCarousel = ({ speakers }) => {
   var settings = {
-    dots: false,
+    dots: true,
     arrows: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 3.5,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
   return (
     <Slider {...settings}>
       {speakers.map((data, index) => (
         <div className="speakers__photo" key={index}>
-          <div className="speakers__photo__wrapper">
-            <img src={data.photo} />
-          </div>
+          <div
+            className="speakers__photo__wrapper"
+            style={{
+              backgroundImage: `url(${data.photo})`,
+            }}
+          ></div>
           <div className="speakers__photo__text">
             <h4 className="speakers__name">{data.name}</h4>
             <p className="speakers__profession">{data.profesi}</p>
